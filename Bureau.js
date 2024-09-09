81889151a73002e8e849e2f554928ee06657e580
Fire_90 <132488525+Fire-90@users.noreply.github.com>
2024-09-09 18:21:40 +0200

    Add files via upload

--- Bureau.js
+++ Bureau.js
@@ -1,4 +1,5 @@
 let currentModal = null;
+let currentArch = null;
 let last_command = '';
 
 document.getElementById('unlock-btn').addEventListener('click', function() {
@@ -99,3 +100,55 @@ function openLog(modalId) {
 
     scrollableArea.scrollTop = scrollableArea.scrollHeight;
 }
+
+
+// Sélection des éléments
+const openArchiveBtn = document.getElementById('openArchiveBtn');
+const closeArchiveBtn = document.getElementById('closeArchiveBtn');
+const archiveInterface = document.querySelector('.archiveInterface');
+const folderButtons = document.querySelectorAll('.folder-btn');
+const selectedArchiveDiv = document.getElementById('selectedArchive');
+const archiveContentDiv = document.querySelector('.archive-content');
+const closeContentBtn = document.querySelector('.archive-content .close');
+
+// Afficher l'interface des archives au clic sur le bouton
+openArchiveBtn.addEventListener('click', () => {
+    archiveInterface.classList.remove('hidden');
+    openArchiveBtn.classList.add('hidden');
+});
+
+// Fermer l'interface des archives au clic sur le bouton "Fermer"
+closeArchiveBtn.addEventListener('click', () => {
+    archiveInterface.classList.add('hidden');
+    openArchiveBtn.classList.remove('hidden');
+});
+
+document.querySelectorAll('.folder-btn').forEach(button => {
+    button.addEventListener('click', function() {
+        const archiveId = this.getAttribute('data-archive');
+        OpenArch(archiveId);
+    });
+});
+
+function OpenArch(ArchId) {
+    if (currentArch) {
+        currentArch.classList.add('hidden'); // Fermer l'archive actuelle
+    }
+
+    const Arch = document.getElementById(ArchId);
+
+    if (Arch) {
+        Arch.classList.remove('hidden');
+        currentArch = Arch; // Mettre à jour l'archive actuelle
+
+        Arch.querySelector('.close').addEventListener('click', function() {
+            Arch.classList.add('hidden');
+            currentArch = null; // Réinitialiser lorsque l'archive est fermée
+        });
+    }
+}
+
+// Fermer l'affichage du contenu de l'archive au clic sur la croix
+closeContentBtn.addEventListener('click', () => {
+    selectedArchiveDiv.classList.add('hidden');
+});
